4aed25003af24a022421c64fa8ee28128ff2c14c
nurtilek02 <nurtilek.azhybekov@mail.ru>
2023-02-22 09:32:11 +0300

    end

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,8 @@
-mport readlineSync from 'readline-sync';
+import readlineSync from 'readline-sync';
 
 const userName = () => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
 };
-export default user
+export default userName;
